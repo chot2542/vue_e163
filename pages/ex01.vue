6ff6b5d51e8a82f376ddf4858325a8376b3c81d4
chot2542 <aekachotzawza1@hotmail.com>
2020-07-08 15:34:20 +0700

    send

--- pages/ex01.vue
+++ pages/ex01.vue
@@ -4,6 +4,11 @@
     <h2>age: {{ age }}</h2>
     <input type="text" :value="name">
     <input type="text" v-model="name">
+    <center>
+    <div><button @click="doSave">SAVE</button></div>
+    <div><button @click="doMenu">MAIN MENU</button></div>
+    <div><button @click="doHome">HOME</button></div>
+    </center>
   </div>
 </template>
 <script>
@@ -14,5 +19,16 @@ export default {
       age: 20,
     }
   },
+  methods:{
+      doSave(){
+      console.log("DO SAVE");    
+      },
+      doMenu(){
+      this.$router.replace('menu');  
+      },
+      doHome(){
+      this.$router.push('/');      
+      },
+    },
 }
 </script>
