85a091e4259d1b1b8ef3d29346dee0e4134a3a85
chot2542 <aekachotzawza1@hotmail.com>
2020-07-21 16:31:41 +0700

    send

---
+++ layouts/dehomework.vue
@@ -0,0 +1,109 @@
+<template>
+    <!-- App.vue -->
+
+<v-app>
+
+    <v-system-bar
+          color="#708090"
+          :height="height"
+          :lights-out="lightsOut"
+          :window="window"
+        >
+          <v-icon>mdi-signal-cellular-outline</v-icon>
+          <span>AIS</span>
+          &nbsp;<v-icon>mdi-wifi-strength-4</v-icon>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <span>12:30</span>
+          <v-spacer></v-spacer>
+          <v-icon>mdi-battery</v-icon>
+          <span>100%</span>
+        </v-system-bar>
+        
+  <v-navigation-drawer app>
+    <!-- -->
+  </v-navigation-drawer>
+
+<div>
+       <v-app-bar color="#fccd14"densedark>
+
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+
+      <template v-slot:extension>
+        <v-tabs align-with-title>
+          <v-tab><b>HOME</b></v-tab>
+          <v-tab><b>PROFILE</b></v-tab>
+          <v-tab><b>LOGIN</b></v-tab>
+        </v-tabs>
+      </template>
+    </v-app-bar>
+</div>
+  <!-- Sizes your content based upon application components -->
+
+
+    <!-- Provides the application the proper gutter -->
+    <v-content>
+      <nuxt/>
+  </v-content>
+  
+  
+ <v-footer color="#fccd14"
+    dark
+    padless
+  >
+    <v-card 
+      flat
+      tile
+      class="indigo lighten-1 white--text text-center"
+    >
+      <v-card-text>
+        <v-btn 
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4 white--text"
+          icon
+        >
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-text>
+
+      <v-card-text class="white--text pt-0">
+        ****************  Chaiyaphum Technical College  ****************
+        ****************************************************************
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+    
+    </v-card>
+  </v-footer>
+</v-app>
+</template>
+<script>
+  export default {
+    data: () => ({
+      icons: [
+        'mdi-facebook',
+        'mdi-twitter',
+        'mdi-linkedin',
+        'mdi-instagram',
+      ],
+    }),
+  }
+</script>
+
---
+++ pages/homework.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-card>
+       <center>
+           <h2>PROFILE</h2>
+           <img src="https://scontent.fbkk12-4.fna.fbcdn.net/v/t1.0-9/101810809_2726154157603880_616036919214984760_o.jpg?_nc_cat=103&_nc_sid=a4a2d7&_nc_eui2=AeHppxnylcorQGbt0HyaXhtV7jZlHojc7qPuNmUeiNzuo--_SceyQ1dNx6jwZmo3jJ5V-6ygV4Xr_cOpjoz36WUR&_nc_ohc=vBixMxrHbEgAX_n6eqk&_nc_ht=scontent.fbkk12-4.fna&oh=6b06eaf3f9b6758525acbda2662b87e4&oe=5F3D8138" 
+           width="150">
+           <p>นายเอกโชติ  งามสมบัติ</p>
+           <p>เกิดวันที่ 2 ธันวาคม 2542 อายุ 20 ปี</p>
+           <p>กำลังศึกษาอยู่ที่ วิทยาลัยเทคนิคชัยภูมิ (ปวส.)</p>
+           <p>Tel.0828532050 FB:โชด'บาร์ line:chot4150</p>
+           <br>
+           <br>
+       </center>
+    </v-card>
+</template>
+<script>
+export default {
+      layout:"dehomework",
+}
+</script>
